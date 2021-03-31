2cc310c67405eb0124314104039e495b0e0d03be
Bell Estrella <bilstar0711@gmail.com>
2021-03-31 11:08:33 -0500

    cambio vista login

---
+++ wapp.gt/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data.gra;
+using Logic.gra;
+using Models.gra;
+
+namespace Wapp.gt.Controllers
+{
+    public class AccountController : Controller
+    {
+   
+        public IActionResult Login()
+        {
+            return View();
+        }
+   
+    }
+}
